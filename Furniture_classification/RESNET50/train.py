ae17090544cd2127a8a735009af2b03b3e3e44d6
Mohit Gupta <mohit.gupta@quantiphi.com>
2019-05-27 18:01:39 +0530

    resnet changes

--- Furniture_classification/RESNET50/train.py
+++ Furniture_classification/RESNET50/train.py
@@ -8,23 +8,21 @@ import tensorflow as tf
 # Training parameters
 
 img_size = 224
+classes = ['chairs', 'curtains','sofas','wardrobes']
 
-def batch_gen(path, batch_size):
+def batch_gen(train_path, valid_path,batch_size):
 
+    train_batches = tf.keras.preprocessing.image.ImageDataGenerator().flow_from_directory(train_path,
+                                                                                 target_size = (img_size,img_size),
+                                                                                 batch_size = batch_size,
+                                                                                 classes = classes)
 
-        data_path = path
+    valid_batches = tf.keras.preprocessing.image.ImageDataGenerator().flow_from_directory(valid_path,
+                                                                         target_size = (img_size,img_size),
+                                                                         batch_size = 10,
+                                                                         classes = classes)
 
-        train_batches = tf.keras.preprocessing.image.ImageDataGenerator().flow_from_directory(data_path,
-                                                                                     target_size = (img_size,img_size),
-                                                                                     batch_size = batch_size,
-                                                                                     classes = ['defective', 'nondefective'])
-
-        valid_batches = tf.keras.preprocessing.image.ImageDataGenerator().flow_from_directory('cropped_data/validation',
-                                                                             target_size = (img_size,img_size),
-                                                                             batch_size = 10,
-                                                                             classes = ['defective', 'nondefective'])
-
-        return train_batches, valid_batches
+    return train_batches, valid_batches
 
 
 def create_model():
@@ -32,7 +30,7 @@ def create_model():
         image_input = tf.keras.layers.Input(shape=(224,224,3))
         model = tf.keras.applications.ResNet50(include_top = True, weights ='imagenet')
 
-        num_classes = 2
+        num_classes = len(classes)
 
         last_layer = model.layers[-2].output # grabing 2nd last layer output
         out = tf.keras.layers.Dense(num_classes, activation = 'softmax', name = 'output')(last_layer)
@@ -46,9 +44,9 @@ def create_model():
         return custom_resnet_model
 
 
-def model_train(path, size, batch_size, epochs):
+def model_train(train_path, valid_path, size, batch_size, epochs):
 
-        train_batches, valid_batches = batch_gen(path, batch_size)
+        train_batches, valid_batches = batch_gen(train_path, valid_path, batch_size)
 
         custom_resnet_model = create_model()
         t1 = time.time()
@@ -62,7 +60,8 @@ def model_train(path, size, batch_size, epochs):
 
 if __name__ == '__main__':
         parser = argparse.ArgumentParser()
-        parser.add_argument('--path', help = 'path of training data.')
+        parser.add_argument('--train_path', help = 'path to training data.')
+        parser.add_argument('--valid_path', help = 'path to validation data.')
         parser.add_argument('--size', help = 'total number of training samples', type = int)
         parser.add_argument('--batch', help = 'number of samples per training batch', default = 64, type = int)
         parser.add_argument('--epochs', help = 'number of epochs for training', default = 5, type = int)
@@ -70,8 +69,9 @@ if __name__ == '__main__':
 
         args = vars(parser.parse_args())
 
-        model_train(args['path'], args['size'], args['batch'], args['epochs'])
+        model_train(args['train_path'],args['valid_path'] args['size'], args['batch'], args['epochs'])
 
 
 # Usage 
-# python3 resnet.py --path <path of training data(data separated in class folders)> --size <dataset size> --batch <batch_size> --epochs <no_of_epochs> 
+# python3 train.py --train_path <path of training data(data separated in class folders)> --valid_path <path of validation data(data separated in class folders)> 
+# --size <dataset size> --batch <batch_size> --epochs <no_of_epochs> 
