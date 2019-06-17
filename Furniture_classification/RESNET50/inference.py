1ad9fc2d110e5a80d8b4ea868419f4e3761b0a71
Mohit Gupta <mohit.gupta@quantiphi.com>
2019-06-17 19:12:01 +0530

    accuracy score added

--- Furniture_classification/RESNET50/inference.py
+++ Furniture_classification/RESNET50/inference.py
@@ -8,20 +8,35 @@ import create_model as cm
 
 classes = ['chairs', 'curtains','sofas','wardrobes']
 
+
+def calculate_accuracy(y, y_):
+	matching_labels = np.count_nonzero(y_ == y)
+	accuracy = matching_labels/len(y_)
+	return accuracy
+
 def inference(path, weights_file):
+
+	category = path.split('/')[-1]
 
 	model = cm.create_model(weights_file)
 	
 	t1 = time.time()
 	counter = 0
+	prediction_list = []
 	for files in os.listdir(path):
 		img = cv2.imread(os.path.join(path, files))
 		img = cv2.resize(img, (224,224))
 		img = np.expand_dims(img, 0)
 		prediction = model.predict(img)
-		prediction_label = classes[np.argmax(prediction)]
-		print(files, prediction_label)
+		# prediction_label = classes[np.argmax(prediction)]
+		prediction_list.append(np.argmax(prediction))
+		# print(files, prediction_label)
 		counter += 1
+
+	# Ground label
+	y = np.full((1, counter), classes.index(category))
+	accuracy = calculate_accuracy(y, np.array(prediction_list))
+	print('Accuracy:', accuracy)
 
 	t2 = time.time()
 	print('Average prediction time:', (t2-t1)/counter)
